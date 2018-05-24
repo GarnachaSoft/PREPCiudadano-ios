546783424fab444913f7061e0ee3568542aea3ea
Juan Villalvazo <seiclick.info@gmail.com>
2018-05-23 21:58:00 -0700

    Se agrego la clase de collectionViewCell, sus funciones en la clase MainManu, la implementación del collectionView y una imagen de prueba

---
+++ PrepCiudadanoMexico/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  PrepCiudadanoMexico
+//
+//  Created by Juan Villalvazo on 23/05/18.
+//  Copyright © 2018 Juan Villalvazo. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imagen: UIImageView!
+    @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var descripcion: UILabel!
+    
+
+
+
+}
--- PrepCiudadanoMexico/MainManuVC.swift
+++ PrepCiudadanoMexico/MainManuVC.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class MainManuVC: UIViewController {
-
+class MainManuVC: UIViewController, UICollectionViewDataSource,UICollectionViewDelegate {
+   
+    
+    let imagenes = [UIImage(named:"tutoriales de programacion")]
+    let titulos = ["Colaborar","Encuentra tu casilla","Resultados","PREP","Incidencias","Donativos","Sobre Nosotros"]
+    let descripciones = ["Su apoyo es importante","Encuentre la casilla mas cercana","Consulta los resultados en tiempo real","Sobre el PREP","Incidencias en tiempo real","Tu aporte hace la diferencia","Nuestro equipo de trabajo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return titulos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // Recuperamos nuestra celda del storyboard como hija de la clase CollectionViewCell y le damos sus atributos.
+        let celda = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! CollectionViewCell
+        
+        celda.imagen.image = imagenes[0]
+        celda.titulo.text = titulos[indexPath.row]
+        celda.descripcion.text = descripciones[indexPath.row]
+        
+        
+        //Crea sombras y modifica la apariencia de nuestra celda
+        celda.contentView.layer.cornerRadius = 4.0
+        celda.contentView.layer.borderWidth = 1.0
+        celda.contentView.layer.borderColor = UIColor.clear.cgColor
+        celda.contentView.layer.masksToBounds = false
+        celda.layer.shadowColor = UIColor.gray.cgColor
+        celda.layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        celda.layer.shadowRadius = 4.0
+        celda.layer.shadowOpacity = 1.0
+        celda.layer.masksToBounds = false
+        celda.layer.shadowPath = UIBezierPath(roundedRect: celda.bounds, cornerRadius: celda.contentView.layer.cornerRadius).cgPath
+        
+        
+        return celda
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
